23c79f35769f132aba7537c57fcc3b7dfaf3f57a
stoe4real <stoe585@yahoo.com>
2025-11-08 15:01:41 -0800

    Add correct SolvleApplication.java

--- src/main/java/com/appsoil/solvle/SolvleApplication.java
+++ src/main/java/com/appsoil/solvle/SolvleApplication.java
@@ -1,15 +1,11 @@
-package com.appsoil.solvle;
+package com.appsoil.solve;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cache.annotation.EnableCaching;
 
-@EnableCaching
 @SpringBootApplication
 public class SolvleApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(SolvleApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(SolvleApplication.class, args);
+    }
 }
