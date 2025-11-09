a27c9e5fb138cf128d78ae017cd0e22b2a2143e9
stoe4real <stoe585@yahoo.com>
2025-11-08 23:48:08 -0800

    Create CorsConfig.java

---
+++ src/main/java/com/appsoil/solvle/CorsConfig.java
@@ -0,0 +1,10 @@
+package com.appsoil.solve;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+    @Override
+    public void add
