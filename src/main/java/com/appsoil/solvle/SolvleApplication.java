6cb6ff8eeff55451bd4bb24ee6d6acc92cd795cb
stoe4real <stoe585@yahoo.com>
2025-11-09 09:16:49 -0800

    Update SolvleApplication.java

--- src/main/java/com/appsoil/solvle/SolvleApplication.java
+++ src/main/java/com/appsoil/solvle/SolvleApplication.java
@@ -1,9 +1,11 @@
-package com.appsoil.solve;
+package com.appsoil.solvle;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "com.appsoil.solvle")
 public class SolvleApplication {
     public static void main(String[] args) {
         SpringApplication.run(SolvleApplication.class, args);
