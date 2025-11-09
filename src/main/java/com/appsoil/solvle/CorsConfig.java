496f998dfa0655283de9c04d6980ca01ba956325
stoe4real <stoe585@yahoo.com>
2025-11-09 00:02:43 -0800

    Update CorsConfig.java

--- src/main/java/com/appsoil/solvle/CorsConfig.java
+++ src/main/java/com/appsoil/solvle/CorsConfig.java
@@ -1,4 +1,4 @@
-package com.appsoil.solve;
+package com.appsoil.solvle;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -7,4 +7,10 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class CorsConfig implements WebMvcConfigurer {
     @Override
-    public void add
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*");
+    }
+}
