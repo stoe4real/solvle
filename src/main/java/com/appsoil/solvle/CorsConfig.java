9fe3e1c82e82dacc863c8503b394a56e2dc4dc60
stoe4real <stoe585@yahoo.com>
2025-11-09 00:18:53 -0800

    Update CorsConfig.java

--- src/main/java/com/appsoil/solvle/CorsConfig.java
+++ src/main/java/com/appsoil/solvle/CorsConfig.java
@@ -9,7 +9,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("*")
+                .allowedOrigins("https://frontend-production-f3f0.up.railway.app")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*");
     }
